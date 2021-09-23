ccf8921109b43d9fefef2e88567adcc680fa1a40
Nathan Yang <nathan.yang16@gmail.com>
2021-09-23 19:49:49 -0400

    Used sass/css to style login page. Added greeting to user who signs in on task display page.

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -1,35 +1,59 @@
 import React from 'react';
-import '../stylesheets/styles.scss';
+import '../stylesheets/Login.scss';
+import '../stylesheets/Container.scss';
 
 class LogIn extends React.Component {
-  constructor(props){
-    super(props);
-  }
-  render() {
-    // console.log(`this is login props ${this.props.addUser}`);
-    return (
-      <div>
-        Username: <input type ="text" id="usernameInput" /><br />
-        Password: <input type="password" id="passwordInput" /><br />
-      {/* trigger an action to POST input from username and password to backend
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		// console.log(`this is login props ${this.props.addUser}`);
+		return (
+			<div id='loginInput'>
+				<h1>ScrumFlow</h1>
+				<div className='loginInfoContainer'>
+					<div className='inputStyle'>
+						<div className='loginInfo'>Username:</div>
+						<div>
+							<input className='inputBox' type='text' id='usernameInput' />
+						</div>
+					</div>
+					<div className='inputStyle'>
+						<div className='loginInfo'>Password:</div>
+						<div>
+							<input className='inputBox' type='password' id='passwordInput' />
+						</div>
+					</div>
+				</div>
+
+				<button
+					id='passwordButton'
+					onClick={() => {
+						return this.props.addUser(
+							document.getElementById('usernameInput').value,
+							document.getElementById('passwordInput').value
+						);
+					}}
+				>
+					Sign up
+				</button>
+				{/* trigger an action to POST input from username and password to backend
       
       */}
-        <button onClick={() => {
-          return this.props.addUser(document.getElementById('usernameInput').value, document.getElementById('passwordInput').value);
-        }}>Sign up
-          
-        </button>
-      {/* trigger an action to POST input from username and password to backend
-      
-      */}
-        <button
-          onClick={() => {
-            return this.props.login(document.getElementById('usernameInput').value, document.getElementById('passwordInput').value);
-          }}>Log in
-        </button>
-      </div>
-    )
-  }
-};
+				<button
+					id='loginButton'
+					onClick={() => {
+						return this.props.login(
+							document.getElementById('usernameInput').value,
+							document.getElementById('passwordInput').value
+						);
+					}}
+				>
+					Login
+				</button>
+			</div>
+		);
+	}
+}
 
 export default LogIn;
--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -3,47 +3,46 @@ import React from 'react';
 import { connect } from 'react-redux';
 import LogIn from '../components/LogIn';
 import TaskContainer from './TaskContainer';
-import '../stylesheets/styles.scss';
+// import '../stylesheets/styles.scss';
 import * as actions from '../actions/actions';
+import '../stylesheets/Container.scss';
 
-const mapStateToProps = state => (
-  {
-    username: state.tasks.username,
-    loggedIn: state.tasks.loggedIn,
-  }
-);
+const mapStateToProps = (state) => ({
+	username: state.tasks.username,
+	loggedIn: state.tasks.loggedIn,
+});
 
-const mapDispatchToProps = dispatch => (
-  {
-    addUser: (username, password) => {
-      return dispatch(actions.addUser(username, password));
-    },
-    login: (username, password) => {
-      return dispatch(actions.login(username,password));
-    }
-  }
-)
+const mapDispatchToProps = (dispatch) => ({
+	addUser: (username, password) => {
+		return dispatch(actions.addUser(username, password));
+	},
+	login: (username, password) => {
+		return dispatch(actions.login(username, password));
+	},
+});
 
 class MainContainer extends React.Component {
-  constructor(props) {
-    super(props);
-   
-  }
-  render(){
-    console.log(`username in main container`,this.props.username)
-    if (this.props.loggedIn === false) 
-      return(
-        <div>
-          <LogIn addUser={this.props.addUser} login={this.props.login} />
-        </div>
-      );
-    else 
-      return (
-        <div>
-          <TaskContainer checkUser={this.props.login} username={this.props.username} />
-        </div>
-      )
-  }
+	constructor(props) {
+		super(props);
+	}
+	render() {
+		console.log(`username in main container`, this.props.username);
+		if (this.props.loggedIn === false)
+			return (
+				<div id='loginContainerStyle'>
+					<LogIn addUser={this.props.addUser} login={this.props.login} />
+				</div>
+			);
+		else
+			return (
+				<div>
+					<TaskContainer
+						checkUser={this.props.login}
+						username={this.props.username}
+					/>
+				</div>
+			);
+	}
 }
 
-export default connect(mapStateToProps, mapDispatchToProps) (MainContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(MainContainer);
